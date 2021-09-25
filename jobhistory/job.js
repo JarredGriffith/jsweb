cf8b231389b20516d7d17159a98a1968b59340c0
JarredGriffith <Jarredgriffith29@gmail.com>
2021-09-25 10:51:38 -0700

    adding text to work history

---
+++ jobhistory/job.js
@@ -0,0 +1,25 @@
+document.write(`
+
+    <h1>test</h1>
+
+     <p>Front-End Software Development Mentor (Part-Time)                                                                                                                2021 - Present
+     Promineo Tech – Las Vegas                                                                                                                              
+     •	Hold session once per week with each student as needed, to help troubleshoot their projects or assignments and solidify the concepts they are learning in class.
+     •	Able to clearly express software development concepts in a manner that students can understand an able to effectively and efficiently troubleshoot errors.
+     •	Proficient in: Introduction to JavaScript, Front End Technologies (HTML, CSS, Bootstrap, JQuery), and Web App Design with React JS.
+     
+     IT System Security Admin.   (Full-Time)                                                                                                                                             2019 - Present
+     Golden Nugget Hotel/Casino – Las Vegas                                                                                                                              
+     •	Responsible for the account creation, deactivation, and Level 1 & 2 support of user accounts across all IT systems.
+     •	Managed IT Systems which include but are not limited to Microsoft Active Directory, LMS (Agilisys), Ballys, Saflok and managed all IT equipment from mobile devices to credit card terminals, which includes issuance to end users and operating departments.
+     •	Additional responsibilities include training users with devices, maintaining the inventory, and recovering devices. As well as continuing to maintain all responsibilities from my previous position of Hotel Systems Analyst.
+     
+     Hotel Systems Analyst  (Full-Time)                                                                                                                                                          2016 - 2019
+     Golden Nugget Hotel/Casino – Las Vegas 
+     
+     •	Developed custom operational solutions with existing hotel systems, which include but is not limited to: LMS, Microsoft SQL Server, Microsoft SharePoint, Microsoft Power BI, and Microsoft Excel (reporting).
+     •	Developed an in-house system to digitize the dispatching of Bellmen requests from guests or additional Golden Nugget staff members, which is still in use today and has enhanced productivity reporting.
+     
+    </p>
+
+`);
